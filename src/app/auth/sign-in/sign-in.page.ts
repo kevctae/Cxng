83ced01aa3c02ab837bc96567401fbe9ea42031f
Kevin Tae <38150856+kevctae@users.noreply.github.com>
2020-09-20 15:26:38 +0700

    Change ui of all auth pages

--- src/app/auth/sign-in/sign-in.page.ts
+++ src/app/auth/sign-in/sign-in.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
@@ -7,12 +8,21 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./sign-in.page.scss'],
 })
 export class SignInPage implements OnInit {
+  signinForm: FormGroup;
 
   constructor(
     public authService: AuthService,
   ) { }
 
   ngOnInit() {
+    this.signinForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, Validators.required),
+    });
+  }
+
+  onSubmit() {
+    this.authService.SignIn(this.signinForm.value.email, this.signinForm.value.password);
   }
 
 }
