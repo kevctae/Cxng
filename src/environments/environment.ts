513cd1f841965ead862c941990bd33efe66208e4
Kevin Tae <38150856+kevctae@users.noreply.github.com>
2020-09-21 13:05:30 +0700

    Add gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBmBJ1cHK-GQiqZYU88jCgnxwjGKOepdAs",
+    authDomain: "cxng-1.firebaseapp.com",
+    databaseURL: "https://cxng-1.firebaseio.com",
+    projectId: "cxng-1",
+    storageBucket: "cxng-1.appspot.com",
+    messagingSenderId: "542691449569",
+    appId: "1:542691449569:web:0c6657022c050c236bb5a0",
+    measurementId: "G-1NSV41K49X"
+  }
 };
 
 /*
